e34f9757ab58eda56058d45bf6167d9fddb4a7ca
huisunyang <2199352201@qq.com>
2020-08-15 10:24:23 +0800

    update

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ var id
 var token
 app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "X-Requested-With,portal-token,application-id")
+  res.header("Access-Control-Allow-Headers", "X-Requested-With,token,id")
   res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS")
   res.header("X-Powered-By",' 3.2.1')
   res.header("Content-Type", "application/json;charset=utf-8")
   if (req.method !== 'OPTIONS') {
-    applicationId = req.headers['id']
+    id = req.headers['id']
     token = req.headers['token']
     next()
   } else {
