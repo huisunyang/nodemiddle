46a6a5732cadd703d0ac93a6d7e9647eab11c23f
孙慧 <sunhui@sunhuideMacBook-Pro.local>
2020-08-28 22:39:30 +0800

    catch error

--- app.js
+++ app.js
@@ -7,6 +7,25 @@ var https = require('https')
 var fs = require('fs')
 
 var app = express()
+// 添加异常处理
+const Layer = require('express/lib/router/layer');
+
+Object.defineProperty(Layer.prototype, 'handle', {
+  enumerable: true,
+  get() {
+    return this.__handle;
+  },
+  set(fn) {
+    if (fn.length === 4) {
+      this.__handle = fn;
+    } else {
+      this.__handle = (req, res, next) =>
+        Promise.resolve()
+          .then(() => fn(req, res, next))
+          .catch(next);
+    }
+  },
+})
 // graphql 接口调用
 const { createApolloFetch } = require('apollo-fetch');
 
